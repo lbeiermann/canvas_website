82dea004dadf0936997af4c7ffcb7dbeebb9e1b3
LeaBe <lea@beiermann.de>
2024-04-14 13:18:18 +0200

    Custom text

--- src/components/Default.tsx
+++ src/components/Default.tsx
@@ -4,68 +4,42 @@ export function Default() {
       <header>
         Lea Beiermann
       </header>
-      <h1>Hallo! 👋</h1>
+      <h1>Hello there! 👋</h1>
       <p>
-        My research investigates the intersection of computing, human-system
-        interfaces, and emancipatory politics. I am interested in the
-        potential of computing as a medium for thought, as a tool for
-        collective action, and as a means of emancipation.
+        <i>Pssst! Click the button in the top right corner to turn this website 
+	into your personal canvas!</i>
       </p>
 
       <p>
-        My current focus is basic research into the nature of digital
-        organisation, developing theoretical toolkits to improve shared
-        infrastructure, and applying this research to the design of new
-        systems and protocols which support the self-organisation of knowledge
-        and computational artifacts.
+        I'm a tech communicator skilled in professional development, 
+	community building, and collaborative content initiatives. I have experience 
+	developing effective B2B communication strategies, leading web-based crowdsourcing 
+	projects, organizing events, and fostering mutually beneficial partnerships. 
+	My experience in dynamic startup environments has equipped me with the skills to 
+	streamline processes and achieve concrete results.
       </p>
 
       <h1>My work</h1>
       <p>
-        Alongside my independent work I am a researcher at <a href="https://block.science/">Block Science</a> building{' '}
-        <i>knowledge organisation infrastructure</i> and at <a href="https://economicspace.agency/">ECSA</a> working on{' '}
-        <i>computational media</i>. I am also part of the nascent <a href="https://libcomp.org/">Liberatory Computing</a>{' '}
-        collective and a co-organiser of the <a href="https://canvasprotocol.org/">OCWG</a>.
+        I've recently transitioned into the role of ecosystem manager at <a href="https://www.zendis.de/">ZenDiS</a>
+	(Zentrum Digitale Souveränität), focusing on supporting the German public sector 
+	in achieving digital sovereignty through secure and adaptable open-source software solutions.
       </p>
+
+      <p>
+        With my academic background in the history of technology, I bring a deep understanding of its 
+	social context. This knowledge informs my commitment to making technology accessible to all 
+	through clear communication and inclusive practices 🔓
+      </p>	
 
       <h1>Get in touch</h1>
       <p>
-        I am on Twitter <a href="https://twitter.com/OrionReedOne">@OrionReedOne</a>,
-        Mastodon <a href="https://hci.social/@orion">@orion@hci.social</a> and GitHub <a href="https://github.com/orionreed">@orionreed</a>. You can also shoot me an email <a href="mailto:me@orionreed.com">me@orionreed.com</a>
+        I am on <a href="https://www.linkedin.com/in/lea-beiermann/">LinkedIn</a> and
+        Mastodon <a href="https://techhub.social/@lea_bei">@lea_bei@techhub.social</a>. You can also send me an email via <a href="mailto:lea@beiermann.de">lea@beiermann.de</a>
       </p>
 
       <span className="dinkus">***</span>
 
-      <h1>Talks</h1>
-      <ol reversed>
-        <li><a
-          href="artifact/causal-islands-integration-domain.pdf">Spatial
-          Canvases: Towards an Integration Domain for HCI @ Causal Islands LA</a></li>
-        <li><a
-          href="https://www.youtube.com/watch?v=-q-kk-NMFbA">Knowledge Organisation Infrastructure Demo @ NPC
-          Denver</a></li>
-      </ol>
-      <h1>Writing</h1>
-      <ol reversed>
-        <li><a
-          href="https://blog.block.science/objects-as-reference-toward-robust-first-principles-of-digital-organization/">Objects
-          as Reference: Toward Robust First Principles of Digital Organization</a></li>
-      </ol>
-      <h1>Things I made recently</h1>
-      <ol reversed>
-        <li><a
-          href="https://twitter.com/OrionReedOne/status/1772934478421188620">Tiny 3D HTML/DOM viewer (you can paste it into your console)</a>
-        </li>
-        <li><a
-          href="https://twitter.com/OrionReedOne/status/1772271156373905465">DOM to canvas translation (it's that button in the top right of this page)</a>
-        </li>
-        <li><a
-          href="https://github.com/OrionReed/tldraw-graph-layout">Constraint-based graph layout in tldraw</a>
-        </li>
-        <li><a
-          href="https://github.com/OrionReed/tldraw-physics">Physics in tldraw</a>
-        </li>
-      </ol>
     </main>
   );
 }
