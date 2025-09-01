555ae8ba43239c23db6c0c465a1124ea993ca76a
Lea Beiermann <44318410+lbeiermann@users.noreply.github.com>
2025-09-01 09:08:01 +0200

    Update Default.tsx

--- src/components/Default.tsx
+++ src/components/Default.tsx
@@ -12,21 +12,27 @@ export function Default() {
       </p>
 
       <p>
-        Working on the most interesting layer of the tech stack: human-to-human interaction 🤝
+        I'm working on the most interesting layer of the tech stack: human-to-human interaction 🤝
+	  </p>
 
+	  <p>
 		Over the past years, I’ve developed and delivered digital skills trainings, led consulting projects for 
 		public and private sector clients, and helped coordinate multi-country collaborations around sovereign IT solutions. 
 		I’ve also advised on partner and community management strategies, built structured partner journeys from the ground up, 
 		and supported open source adoption through public speaking and stakeholder engagement.
-   
+	  </p>
+
+      <p>
         I'm currently Partnership Lead at <a href="https://www.zendis.de/">ZenDiS</a> (the German Centre for 
 	Digital Sovereignty), where I focus on strengthening the digital sovereignty of the public sector through 
 	secure and adaptable open source software solutions.
-
+	  </p>
+      <p>
     With a background in the history of technology, I approach digital transformation with a critical understanding of how 
 		  technologies evolve, who they serve, and what values they reflect. This perspective helps me communicate technical 
 		  topics in a way that is context-aware and inclusive.
-
+      </p>
+      <p>
 Outside of work, I spend my time kayaking and visiting art museums — both ways to slow down, observe more closely, and see familiar things from a new angle 🛶🖼️
       </p>	
 
@@ -40,7 +46,8 @@ Outside of work, I spend my time kayaking and visiting art museums — both ways
       </p>
 
       <p>
-        <i>This playful website was designed by <a href="https://github.com/OrionReed/orionreed.com">Orion Reed</a>.</i>
+        <i>Pssst! Click the button in the top right corner to turn this website 
+	into your personal canvas! This playful website was designed by <a href="https://github.com/OrionReed/orionreed.com">Orion Reed</a>.</i>
       </p>
 
     </main>
